7c90b918a89757287c3e408371542b1e67fe95a7
driwach <mohamed@driwach.com>
2021-05-30 14:13:27 +0200

    Finished

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,9 @@ import { UserService } from './../core/services/user.service';
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { OkdialogComponent } from 'src/app/shared/dialogs/okdialog/okdialog.component';
+import { MatDialog } from '@angular/material/dialog';
+
 /** define component */
 @Component({
   selector: 'app-login',
@@ -16,9 +19,13 @@ export class LoginComponent {
    *constructor
    * @param fb form builder
    * @param userService user service
-   *
+   * @param dialog the okdialog
    */
-  constructor(private fb: FormBuilder, private userService: UserService) {}
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    public dialog: MatDialog
+  ) {}
   /** handle hide and show of password in template */
   hide = true;
   /** form of login */
@@ -38,15 +45,31 @@ export class LoginComponent {
     const email = this.userLogin.get('email')?.value;
     const password = this.userLogin.get('password')?.value;
     this.userService.login(email, password).subscribe((res) => {
-      const user = res.user;
-      localStorage.setItem('token', res.access_token);
-      localStorage.setItem('name', user.name);
-      localStorage.setItem('adress', user.adress);
-      localStorage.setItem('email', user.email);
-      localStorage.setItem('id', String(user.id));
-      localStorage.setItem('latitude', user.latitude);
-      localStorage.setItem('longitude', user.longitude);
-      localStorage.setItem('phone', user.phone);
+      if (res === '@') {
+        this.openDialog();
+      } else {
+        const user = res.user;
+        localStorage.setItem('token', res.access_token);
+        localStorage.setItem('name', user.name);
+        localStorage.setItem('adress', user.adress);
+        localStorage.setItem('email', user.email);
+        localStorage.setItem('id', String(user.id));
+        localStorage.setItem('latitude', user.latitude);
+        localStorage.setItem('longitude', user.longitude);
+        localStorage.setItem('phone', user.phone);
+        location.reload();
+      }
+    });
+  }
+
+  /**
+   *  will navigate to products of the selected shop and will pass the id by url
+   */
+  openDialog() {
+    let dialogRef = this.dialog.open(OkdialogComponent);
+    dialogRef.componentInstance.title = 'Error';
+    dialogRef.componentInstance.content = 'Error in email or password!';
+    dialogRef.afterClosed().subscribe((res) => {
       location.reload();
     });
   }
