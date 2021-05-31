2b7d7501dd8af18348918ece8ee51c5f812ff5ce
driwach <mohamed@driwach.com>
2021-05-31 23:33:41 +0200

    fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   /** this to define the production  */
   production: true,
   /** here the Api url */
-  API_URL: 'https://alavora.cat/api',
+  API_URL: 'https://alavora.cat/api/', //
 };
